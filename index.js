a8c1156bf71f89d7bbc25a6fffe89eb169de4e99
admin <linwenjun@163.com>
2019-05-24 06:57:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// 阅读以上推荐链接，了解所有的JavaScript字符串方法。
+
+
+// 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
+var name = 'hello';
+// TODO write your code here...
+
+
+// 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
+var sentence = 'good afternoon, mr mike.';
+//TODO write your code here...
+
+
+// 完成下面程序，将money变量中的金额提取出来，输出：20。
+var money = '￥20';
+//TODO write your code here...
